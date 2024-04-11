1ad78f48316ac0364ae7610fbb66552bbeef08ef
Sudipta987 <rajakar21162@gmail.com>
2024-04-11 20:16:12 +0530

    Q2 : W.A.P to print a pattern2 by using loops

---
+++ PatternBasic2_Q2.java
@@ -0,0 +1,23 @@
+/*W.A.P to print a pattern2 by using loops 
+* * * *
+* * *
+* *
+*
+*/
+package Java.Print_Patterns;
+import java.util.Scanner;
+public class PatternBasic2_Q2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the pattern length n(row=col): ");
+        int n=sc.nextInt();
+        System.out.println("The Pattern is -> ");
+        for(int i=n;i>=1;i--) {
+            for(int j=1;j<=i;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
